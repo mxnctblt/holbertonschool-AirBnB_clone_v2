dd94ae33e242ce906c039aa2ec5cd46beeb21a73
root <root@pc>
2023-03-22 13:49:16 +0100

    syntax fix

--- models/review.py
+++ models/review.py
@@ -9,5 +9,5 @@ class Review(BaseModel):
     """ Review classto store review information """
     __tablename__ = 'reviews'
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.is'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
