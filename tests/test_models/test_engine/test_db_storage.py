44d45d5d9b17db86925d8918ba642f3edc57102c
root <root@pc>
2023-03-24 13:50:50 +0100

    edit the test_all

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -14,79 +14,78 @@ class TestDBStorage(unittest.TestCase):
     """Tests the DBStorage class"""
 
     
-    def setUp(cls):
-        '''
-            Sets up the environment for testing DBStorage
-        '''
-        """Setup the class"""
-        cls.user = User()
-        cls.user.first_name = "Betty"
-        cls.user.last_name = "Holberton"
-        cls.user.email = "Betty@mail.com"
-        cls.user.password = "hbtndev"
+    # def setUp(cls):
+    #     '''
+    #         Sets up the environment for testing DBStorage
+    #     '''
+    #     """Setup the class"""
+    #     cls.user = User()
+    #     cls.user.first_name = "Betty"
+    #     cls.user.last_name = "Holberton"
+    #     cls.user.email = "Betty@mail.com"
+    #     cls.user.password = "hbtndev"
 
-    @classmethod
-    def teardown(cls):
-        """at the end of the test this will tear it down"""
-        del cls.user
+    # @classmethod
+    # def teardown(cls):
+    #     """at the end of the test this will tear it down"""
+    #     del cls.user
 
-    def tearDown(self):
-        """teardown"""
-        pass
+    # def tearDown(self):
+    #     """teardown"""
+    #     pass
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        def test_all(self):
-            """Tests all method"""
-            storage = DBStorage()
-            self.assertIsInstance(storage, DBStorage)
+    def test_all(self):
+        """Tests all method"""
+        storage = DBStorage()
+        self.assertIsInstance(storage, DBStorage)
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        def test_new(self):
-            """Tests new method"""
-            storage = DBStorage()
-            obj = storage.all()
-            user = User()
-            user.id = 123455
-            user.first_name = "Liam"
-            user.last_name = "Stone"
-            user.email = "liamstone@gmail.com"
-            user.password = "sdf55"
-            storage.new(user)
-            key = user.__class__.__name__ + "." + str(user.id)
-            if os.environ['HBNB_TYPE_STORAGE'] != 'db':
-                self.assertIsNotNone(obj[key])
+    # if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    #     def test_new(self):
+    #         """Tests new method"""
+    #         storage = DBStorage()
+    #         obj = storage.all()
+    #         user = User()
+    #         user.id = 123455
+    #         user.first_name = "Liam"
+    #         user.last_name = "Stone"
+    #         user.email = "liamstone@gmail.com"
+    #         user.password = "sdf55"
+    #         storage.new(user)
+    #         key = user.__class__.__name__ + "." + str(user.id)
+    #         if os.environ['HBNB_TYPE_STORAGE'] != 'db':
+    #             self.assertIsNotNone(obj[key])
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        def test_save(self):
-            """Test save method."""
-            st = State(name="California")
-            self.storage.new(st)
-            store = list(self.storage._DBStorage__session.new)
-            self.assertIn(st, store)
+    # if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    #     def test_save(self):
+    #         """Test save method."""
+    #         st = State(name="California")
+    #         self.storage.new(st)
+    #         store = list(self.storage._DBStorage__session.new)
+    #         self.assertIn(st, store)
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        def test_delete(self):
-            """ Tests delete method
-            """
-            st = State(name="New_York")
-            storage.__session.add(st)
-            self.storage.__session.commit()
-            self.storage.delete(st)
-            self.assertIn(st, list(self.storage._DBStorage__session.deleted))
+    # if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    #     def test_delete(self):
+    #         """ Tests delete method
+    #         """
+    #         st = State(name="New_York")
+    #         storage.__session.add(st)
+    #         self.storage.__session.commit()
+    #         self.storage.delete(st)
+    #         self.assertIn(st, list(self.storage._DBStorage__session.deleted))
 
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        def test_reload_DBStorage(self):
-            """
-            Tests reload method
-            """
-            self.storage.save()
-            root = os.path.dirname(os.path.abspath("console.py"))
-            path = os.path.join(root, "file.json")
-            x = self.storage._DBStorage__session
-            self.storage.reload()
-            y = self.storage._DBStorage__session
-            self.assertNotEqual(x, y)
+    # if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    #     def test_reload_DBStorage(self):
+    #         """
+    #         Tests reload method
+    #         """
+    #         self.storage.save()
+    #         root = os.path.dirname(os.path.abspath("console.py"))
+    #         path = os.path.join(root, "file.json")
+    #         x = self.storage._DBStorage__session
+    #         self.storage.reload()
+    #         y = self.storage._DBStorage__session
+    #         self.assertNotEqual(x, y)
 
 
 if __name__ == "__main__":
