90336aa0c2914ea9564c78b476d7aec058fc3c69
mxnctblt <thibaultmaxence44@gmail.com>
2023-04-04 03:30:43 -0700

    improve engines

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,3 +61,7 @@ class FileStorage:
         if obj:
             key = str(obj.__class__.__name__) + '.' + str(obj.id)
             del self.__objects[key]
+
+    def close(self):
+        """ call reload() method for deserializing the JSON file to objects """
+        self.reload()
